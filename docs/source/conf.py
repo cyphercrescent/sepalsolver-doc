6c77e717e71aeecf868d224f84462565bad1edaf
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2025-05-10 07:21:51 +0100

    Lumache to CypherCrescent

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CypherCresent'
+copyright = '2025, CypherCrescent'
+author = 'CypherCrescent'
 
 release = '0.1'
 version = '0.1.0'
